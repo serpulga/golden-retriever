59262d521b1851f212baf2c48fde58e72b52d902
Sergio Pulgarin <sergio.pulgarin@gmail.com>
2013-02-17 14:54:32 -0500

    Fixing indentation

--- goldenretriever/retrieve/static/js/retrieve.js
+++ goldenretriever/retrieve/static/js/retrieve.js
@@ -1,43 +1,42 @@
 $(document).ready(function() {
-	$('#inpterror').html('').hide();
-	
-	$('#retrbttn').click(function(e) {
-		e.preventDefault();
-		
-		/* Clearing previous state before retrieving
-		   new data. */
+    $('#inpterror').html('').hide();
+
+    $('#retrbttn').click(function(e) {
+        e.preventDefault();
+
+        /* Clearing previous state before retrieving
+           new data. */
         $('#titleretr').html('');
         $('#imgretr').attr('src', '').hide();
         $('#inpterror').html('').hide();
-		
-		var baseUrl = '/retrieve/?url='
-		var siteurl = baseUrl + $('#url_input').val();
-		
-		if (siteurl == baseUrl) {
-			/* Happens when text input field is empty. */
-			$('#inpterror').html('Enter a valid URL').show();
-			return
-		}
-		
-		$.ajax({
-	        success: function(data) {
-	        	if (data.error == 'true') {
-	        		$('#inpterror').html(data.msg).show();	
-	        	}
-	        	
-	        	else {
-	        		/* Updates fields with information inside the
-	        		   returned JSON. */
-		            $('#imgretr').attr('src', data.imgsrc).show();
-		            $('#titleretr').html(data.title);
-	        	}
-	        },
-	        error: function(data) {
-	        	$('#inpterror').html('An error occurred').show();	
-	        },
-	        url: siteurl
-		});
-		
-	});
+
+        var baseUrl = '/retrieve/?url='
+        var siteurl = baseUrl + $('#url_input').val();
+
+        if (siteurl == baseUrl) {
+            /* Happens when text input field is empty. */
+            $('#inpterror').html('Enter a valid URL').show();
+            return
+        }
+
+        $.ajax({
+            success: function(data) {
+                if (data.error == 'true') {
+                    $('#inpterror').html(data.msg).show();	
+                }
+
+                else {
+                    /* Updates fields with information inside the
+                       returned JSON. */
+                    $('#imgretr').attr('src', data.imgsrc).show();
+                    $('#titleretr').html(data.title);
+                }
+            },
+            error: function(data) {
+                $('#inpterror').html('An error occurred').show();	
+            },
+            url: siteurl
+        });
+    });
 });
 
